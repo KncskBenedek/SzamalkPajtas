522426f5400c0f577bed18bb6c574d373b527d1b
KncskBenedek <koncsikbenedek@gmail.com>
2023-03-19 17:35:05 +0100

    alap db script, modellek legenerálva backenden

---
+++ PajtasBackend/Models/Halmazertekek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PajtasBackend.Models;
+
+public partial class Halmazertekek
+{
+    public short Tulajd { get; set; }
+
+    public short Sorsz { get; set; }
+
+    public string Ertek { get; set; } = null!;
+
+    public virtual ICollection<Sajat> Sajats { get; } = new List<Sajat>();
+
+    public virtual BovTulaj TulajdNavigation { get; set; } = null!;
+
+    public virtual ICollection<Felhasznalo> Felhaszns { get; } = new List<Felhasznalo>();
+}
---
+++ PajtasBackend/Models/KerMindenE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PajtasBackend.Models;
+
+public partial class KerMindenE
+{
+    public int FhAzon { get; set; }
+
+    public string Nev { get; set; } = null!;
+
+    public short Tulajd { get; set; }
+
+    public string? Jelentés { get; set; }
+
+    public short Sorsz { get; set; }
+
+    public string Ertek { get; set; } = null!;
+}
